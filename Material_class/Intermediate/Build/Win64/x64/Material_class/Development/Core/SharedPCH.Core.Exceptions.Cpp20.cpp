c970f2612480900382a2df1611c788357ef35fbb
Enrique Orihuela <134656348+CaseBroom65@users.noreply.github.com>
2024-11-12 16:42:38 -0600

    new

---
+++ Material_class/Intermediate/Build/Win64/x64/Material_class/Development/Core/SharedPCH.Core.Exceptions.Cpp20.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.38.33140
+#include "D:/unity/Unreal_Engine_5/Material_class/Intermediate/Build/Win64/x64/Material_class/Development/Core/SharedPCH.Core.Exceptions.Cpp20.h"
---
+++ Material_class/Intermediate/Build/Win64/x64/Material_class/Development/Core/SharedPCH.Core.Exceptions.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Core/Public/CoreSharedPCH.h
+#include "D:/unity/Unreal_Engine_5/Material_class/Intermediate/Build/Win64/x64/Material_class/Development/Core/SharedDefinitions.Core.Exceptions.Cpp20.h"
+#include "Runtime/Core/Public/CoreSharedPCH.h"
